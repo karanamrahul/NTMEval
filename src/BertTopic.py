284b89eff3fa06f80fa2728001f2f467031f355c
Kritika Agarwal <KritikaAgarwal@users.noreply.github.com>
2022-12-17 05:19:14 -0500

    Modifying BertTopic.py to include both hdbscan and agglomerative clustering.

--- src/BertTopic.py
+++ src/BertTopic.py
@@ -5,29 +5,29 @@ import gensim.corpora as corpora
 from gensim.utils import tokenize
 from gensim.models import CoherenceModel
 TOKENIZERS_PARALLELISM = True
+from umap import UMAP
+from sklearn.cluster import AgglomerativeClustering
+from hdbscan import HDBSCAN
 
-
-
-
-
-def bertopic_train(sentences, embedding_model='all-MiniLM-L6-v2'):
-    
-    docs = fetch_20newsgroups(subset='all',  remove=('headers', 'footers', 'quotes'))['data']
-
-    topic_model = BERTopic()
-    topics, probs = topic_model.fit_transform(docs)
+def bertopic_train(sentences,cluster_count,embedding_model='all-MiniLM-L6-v2', model_type='HDBSCAN'):
   
-    model = BERTopic(sentences, workers=4, embedding_model='all-MiniLM-L6-v2',
-    umap_args={'n_neighbors':30, 'n_components':32},
-    hdbscan_args={'min_cluster_size':100, 'min_samples':10})
+    umap_model = UMAP(n_neighbors=15, n_components=32, min_dist=0.0, metric='cosine')
     
-    #embedding_batch_size=32
+    if model_type == "HDBSCAN":
+        model = HDBSCAN(min_cluster_size=15, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
+    elif model_type == "agglomerative":
+        model = AgglomerativeClustering(n_clusters=cluster_count)
+        
+    model = BERTopic(sentences, embedding_model=embedding_model,
+    umap_model=umap_model, hdbscan_model=model)
+    
+    topics, probs = model.fit_transform(sentences)
     return model
 
 def evaluate_bertopic(topic_words,sentences):
     tokenized = [list(tokenize(s)) for s in sentences]
     id2word = corpora.Dictionary(tokenized)
     corpus = [id2word.doc2bow(text) for text in tokenized]
-    cm = CoherenceModel(topics=[s.tolist() for s in topic_words] ,texts=tokenized, corpus=corpus, dictionary=id2word, coherence='c_v')
+    cm = CoherenceModel(topics=[s for s in topic_words] ,texts=tokenized, corpus=corpus, dictionary=id2word, coherence='c_v')
        
     print("Model Coherence C_V is:{0}".format(cm.get_coherence()))
